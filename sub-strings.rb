820f4a48ca633056b6596b8b01d72b6bd500a586
harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-07-07 12:07:41 -0400

    Complete sub-strings

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,10 +1,11 @@
 dictionary = [ "below", "down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+
+
 def substrings(string, dictionary, new_hash = Hash.new)
-    
     sub = dictionary.map do |e|
-        if string[e] == e
-            new_hash[e] = dictionary.count(e)
+        if string.downcase[e.downcase] == e
+            new_hash[e] = string.split(" ").count{ |y| y.downcase[e.downcase] == e}
         end
     end
     p new_hash
