0e3bed2504140f4624fb48e3d071bdc3e0b063ed
harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-07-07 23:53:14 -0400

    Refactor stock-piker

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,21 +1,24 @@
 def stock_picker (array, new_array = Array.new)
-    
-    if array[0] == array.max
-        array.delete_at(0)
-    elsif array[-1] == array.min
-        array.delete_at(-1)
+    #new days array for modification
+    days = [] + array
+
+    if days[0] == days.max
+        days.delete_at(0)
+    elsif days[-1] == days.min
+        days.delete_at(-1)
     end
 
-    if array.index(array.min) > array.index(array.max)
-        array.delete(array.min)
+    if days.index(days.min) > days.index(days.max)
+        days.delete_at(days.index(days.min))
     end
 
-    buy = array.min
-    sell = array.max
+    #days and original array comparison
+    buy = days.min
+    sell = days.max
 
-    p buy
-    p sell
-
+    new_array << array.index(buy)
+    new_array << array.index(sell)
+    p new_array
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
